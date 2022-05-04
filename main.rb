f2effe91d87a1a5c6067b98f2064232d0d31f403
Kenny <kennysalazar3197@gmail.com>
2022-05-04 16:02:19 -0500

    Define decode_char method

--- main.rb
+++ main.rb
@@ -1,13 +1,61 @@
+DICTIONARY = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' =>  'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0',
+}
+
+def decode_char(char)
+  letter = DICTIONARY[char]
+  decoded_sentence += letter
+end
+
 def decode_word(word)
   word_arr = word.split(/ /)
-  word_arr.each | char |
+  word_arr.each do | char |
     decode_char(char)
+  end
+  decoded_sentence += ' '
 end
 
 def decode(string)
+  decoded_sentence = ''
   sentence_arr = string.split(/   /)
-  sentence_arr.each | word |
+  sentence_arr.each do | word |
     decode_word(word)
+  end
 end
 
-'.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decode '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
