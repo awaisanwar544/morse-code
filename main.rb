669b79cd996daebfad070fd16cbd6c848438bdb2
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-05 01:14:03 +0500

    Add decode_char method

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
-def decode (string)
+def decode_word(word)
+  word_arr = word.split(/ /)
+  word_arr.each | char |
+    decode_char(char)
+end
+
+def decode(string)
   sentence_arr = string.split(/   /)
-  sentence_arr.each { |word|
-    decode_word (word)
-  }
+  sentence_arr.each | word |
+    decode_word(word)
 end
 
 '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
